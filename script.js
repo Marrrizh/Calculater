05b907cce361da9708ba3128e3209189b9c02d27
Maria S <mariz87@yandex.ru>
2025-11-03 15:20:43 +0300

    Added display shows operations with numbers

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let display = document.querySelector('.display');
 let numberButtons = document.querySelectorAll('.number');
 let currentNumber = '';
 let emptyDisplay=document.querySelector('.clear')
+let operationButtons = document.querySelectorAll('.operator');
 
 
 numberButtons.forEach(button => {
@@ -16,3 +17,15 @@ emptyDisplay.addEventListener('click', function() {
         currentNumber = "";
         display.textContent = "0";  
     })
+
+    let firstNumber="";
+    
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        let newDigit = button.textContent;
+        currentNumber = currentNumber + newDigit;
+        displayValue = currentNumber;          // Новая переменная!
+        display.textContent = displayValue;    // Показываем её
+    });
+});
