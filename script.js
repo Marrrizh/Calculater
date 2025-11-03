0be00730f83ce73e6e168834bfd4ff61c5dc103f
Maria S <mariz87@yandex.ru>
2025-11-03 16:02:14 +0300

    Added result of operations

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let numberButtons = document.querySelectorAll('.number');
 let currentNumber = '';
 let emptyDisplay=document.querySelector('.clear')
 let operationButtons = document.querySelectorAll('.operator');
+let equalsButton = document.querySelector('.equals');
 
 
 numberButtons.forEach(button => {
@@ -23,9 +24,30 @@ emptyDisplay.addEventListener('click', function() {
 
 operationButtons.forEach(button => {
     button.addEventListener('click', function() {
-        let newDigit = button.textContent;
-        currentNumber = currentNumber + newDigit;
-        displayValue = currentNumber;          // Новая переменная!
-        display.textContent = displayValue;    // Показываем её
-    });
+        currentOperation = button.textContent;
+        firstNumber=currentNumber;
+        currentNumber="";
+        displayValue =  firstNumber+" "+currentOperation+" "+currentNumber;
+         display.textContent = displayValue; 
+
+    }); 
+});
+
+
+equalsButton.addEventListener('click', function() {
+    let num1 = Number(firstNumber);
+    let num2 = Number(currentNumber);
+    let result;
+    
+    if (currentOperation === '+') {
+        result = num1 + num2;
+    } else if (currentOperation === '-') {
+        result = num1 - num2;
+    } else if (currentOperation === '×') {
+        result = num1 * num2;
+    } else if (currentOperation === '÷') {
+        result = num1 / num2;
+    }
+    
+    display.textContent = result;
 });
